74277ce101e9fefffe58ba187e6ad058374a0655
Mervin Fansler <mmfansler@gmail.com>
2024-07-12 20:52:11 +0200

    no test load when cc

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+export DISABLE_AUTOBREW=1
+
 if [[ $(uname) == Darwin ]]; then
-  PKG_LIBS="-L${PREFIX}/lib -headerpad_max_install_names" $R CMD INSTALL --build .
+  export PKG_LIBS="-L${PREFIX}/lib -headerpad_max_install_names"
 else
   # .. on Linux the situation is even worse:
-  LD_LIBRARY_PATH=${PREFIX}/lib/R/lib:${PREFIX}/lib \
-  PKG_LIBS="-L${PREFIX}/lib -L${PREFIX}/lib/R/lib -lR -Wl,-rpath-link,${PREFIX}/lib -lblas" \
-    $R CMD INSTALL --build . 
+  export LD_LIBRARY_PATH=${PREFIX}/lib/R/lib:${PREFIX}/lib
+  export PKG_LIBS="-L${PREFIX}/lib -L${PREFIX}/lib/R/lib -lR -Wl,-rpath-link,${PREFIX}/lib -lblas"
+fi
+
+# Check if we are cross-compiling
+if [ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]; then
+  R CMD INSTALL --build --no-test-load . ${R_ARGS}
+else
+  R CMD INSTALL --build . ${R_ARGS}
 fi
